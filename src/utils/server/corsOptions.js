82bed487b21e0745c71f35dc41c8b86eead6997d
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-04-13 21:05:44 +0200

    update

--- src/utils/server/corsOptions.js
+++ src/utils/server/corsOptions.js
@@ -2,7 +2,7 @@ const whitelist = ['https://all-playlists-fe.herokuapp.com', process.env.CALLBAC
 const corsOptions = {
 	origin: (origin, callback) => {
 		console.log(`Òrigin is` + origin)
-		if (whitelist.indexOf(origin) !== -1) {
+		if (whitelist.indexOf(origin) !== -1 || !origin) {
 			console.log("allowed")
 			callback(null, true);
 		
